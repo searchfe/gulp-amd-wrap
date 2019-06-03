7fbedc250ddba3cd8e64e4b190445bcd2105c353
qiansc <van3008@163.com>
2019-06-03 20:46:27 +0800

    fix:exclude file

---
+++ test/filter.spec.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { include } from '../src/filter';
+
+describe('Filter Test', () => {
+  it('include test', () => {
+    const rs = include(
+      resolve(__dirname, './assert/exclude-define.js'),
+      ['*.js'], {
+      cwd: resolve(__dirname, './assert'),
+    });
+    expect(rs).toBe(true);
+  });
+
+  it('include test', () => {
+    const rs = include(
+      'exclude-define.js',
+      ['*.js'], {
+      cwd: resolve(__dirname, './assert'),
+    });
+    expect(rs).toBe(true);
+  });
+
+});
